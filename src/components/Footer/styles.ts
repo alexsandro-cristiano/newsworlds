55d34e6a766353afb7e4727f804a4678e6b80714
alexsandro-cristiano <acg.alexsandro@hotmail.com>
2022-09-18 08:14:57 -0300

    feet(layout responsive):adiciona responsividade no footer

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,26 +1,40 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 export const Container = styled.footer`
   display: flex;
+  align-items: center;
   justify-content: space-around;
-  align-items: flex-end;
-  flex-wrap: wrap;
-  padding: 2rem 0;
-  height: 9rem;
+  flex-direction: column;
+  height: 18rem;
   color: #fff;
   background: #000;
+  padding: 1rem 0 0;
   .direitos {
-    font-size: 1.6rem;
+    font-size: 1.3rem;
+    text-align: center;
+    order: 2;
   }
   .midiaSocial {
     h3 {
       font-weight: 400;
       font-size: 1.4rem;
-      margin-bottom: 0.8rem;
+      margin-bottom: 1.5rem;
     }
     ul {
       display: flex;
-      gap: 1rem;
+      justify-content: center;
+      gap: 2rem;
     }
   }
-`;
+  @media (min-width: 500px) {
+    flex-wrap: wrap;
+    align-items: flex-end;
+    flex-direction: row;
+    height: 10rem;
+    padding-bottom: 1rem;
+    .direitos {
+      font-size: 1.5rem;
+      order: 0;
+    }
+  }
+`
